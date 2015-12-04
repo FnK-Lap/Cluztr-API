257879c17348c926fb70e7ff991a8ecf5d968b0e
Franck LAPEYRE <fnk.lap@gmail.com>
2015-12-04 22:02:03 +0100

    fix chatModel

--- model/chatModel.js
+++ model/chatModel.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose'),
-    Group     = require('./groupModel'),
+    Group    = require('./groupModel'),
+    Message  = require('./messageModel'),
     Schema   = mongoose.Schema;
 
 var ChatSchema = new Schema({
-	message   : { type: String },
+	messages  : [{ type: Schema.Types.ObjectId, ref: 'Message' }],
 	created_at: { type: Date },
 	group1    : { type: Schema.Types.ObjectId, ref: 'Group' },
 	group2    : { type: Schema.Types.ObjectId, ref: 'Group' },
