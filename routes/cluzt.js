cc4066ce56cdd79f4280c222a2b1096f2e093664
maxime <maxime.dumont@newmanity.com>
2016-06-28 22:22:48 +0200

    route for get all cluzts

--- routes/cluzt.js
+++ routes/cluzt.js
@@ -6,31 +6,52 @@ var _        = require('underscore');
 
 var cluzt = {
     getAllReceive : function (req, res) {
-//       var groupId = req.params.id;
-//       Cluzt.find({receiver.id == groupId && send == true) {
-//         if (err) {
-//           res.send(err)
-//         } else {
-//           async.each(cluzts.sender, function(item, cb) {
-//             Group.populate(item, { path: "sender" }, function(err, output) {
-//               if (err) {
-//                 res.json({
-//                   status:400,
-//                   message: err
-//                 });
-//               }
-//               cb();
-//             });
-//           }, function(err) {
-//             res.json({
-//               status:200,
-//               message: "Get Cluzt success",
-//               data: cluzts
-//             });
-//           }
-//         }
-//       });
+      var groupId = req.params.id;
+      Cluzt.find({receiver: groupId, send: true) {
+        if (err) {
+          res.send(err)
+        } else {
+          async.each(cluzts, function(item, cb) {
+            Group.populate(item, { path: "sender" }, function(err, output) {
+              if (err) {
+                res.json({
+                  status:400,
+                  message: err
+                });
+              }
+              cb();
+            });
+          }, function(err) {
+            res.json({
+              status:200,
+              message: "Get Cluzt success",
+              data: cluzts
+            });
+          }
+        }
+      });
     }
+    // setCluzt : function (req, res) {
+    //   var user = req.Cluztr.user;
+    //   var senderId = req.params.senderId;
+    //   var receiverId = req.params.receiverId;
+    //
+    //   Cluzt.find
+    //   var cluzt = new Cluzt({
+    //     sender: senderId,
+    //     receiver: receiverId,
+    //     acceptedUsers: [user],
+    //     send: false
+    //   });
+    //
+    //   cluzt.save();
+    //
+    //   res.json({
+    //     status: 201,
+    //     message: 'clutz created',
+    //     cluzt: cluzt
+    //   });
+    // }
 }
 
 module.exports = cluzt;
