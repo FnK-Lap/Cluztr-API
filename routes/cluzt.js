b934fb4fd4c554a90379e8fc46ab3a98152dbdab
maxime <maxime.dumont@newmanity.com>
2016-06-28 20:26:28 +0200

    comments function so Fnk can work

--- routes/cluzt.js
+++ routes/cluzt.js
@@ -11,33 +11,33 @@ Comment
     console.log(comments);
   });
 
-var cluzt = {
-    getAllReceive : function (req, res) {
-      var groupId = req.params.id;
-      Cluzt.find({receiver.id == groupId && send == true) {
-        if (err) {
-          res.send(err)
-        } else {
-          async.each(cluzts.sender, function(item, cb) {
-            Group.populate(item, { path: "sender" }, function(err, output) {
-              if (err) {
-                res.json({
-                  status:400,
-                  message: err
-                });
-              }
-              cb();
-            });
-          }, function(err) {
-            res.json({
-              status:200,
-              message: "Get Cluzt success",
-              data: cluzts
-            });
-          }
-        }
-      });
-    }
-}
+// var cluzt = {
+//     getAllReceive : function (req, res) {
+//       var groupId = req.params.id;
+//       Cluzt.find({receiver.id == groupId && send == true) {
+//         if (err) {
+//           res.send(err)
+//         } else {
+//           async.each(cluzts.sender, function(item, cb) {
+//             Group.populate(item, { path: "sender" }, function(err, output) {
+//               if (err) {
+//                 res.json({
+//                   status:400,
+//                   message: err
+//                 });
+//               }
+//               cb();
+//             });
+//           }, function(err) {
+//             res.json({
+//               status:200,
+//               message: "Get Cluzt success",
+//               data: cluzts
+//             });
+//           }
+//         }
+//       });
+//     }
+// }
 
 module.exports = cluzt;
