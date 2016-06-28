25c39a054a794d21bb4674a04b1cbc67e63a9369
maxime <maxime.dumont@newmanity.com>
2016-06-28 20:06:51 +0200

    add get all receive cluzts method

--- routes/cluzt.js
+++ routes/cluzt.js
@@ -4,22 +4,40 @@ var Cluzt    = require('../model/cluztModel.js');
 var User     = require('../model/userModel.js');
 var _        = require('underscore');
 
-// var cluzt = {
-//     getAllReceive : function (req, res) {
-//       var groupId = req.params.id;
-//       Cluzt.find({receiver.id == groupId && send == true, function(err, cluzts) {
-//         if (err) {
-//           res.send(err)
-//         } else {
-//
-//           res.json({
-//             status: 200,
-//             data: cluzts,
-//             message: "Cluzts finds"
-//           });
-//         }
-//       });
-//     },
-// }
-//
-// module.exports = cluzt;
+Comment
+  .find()
+  .populate('author')
+  .exec(function (err, comments) {
+    console.log(comments);
+  });
+
+var cluzt = {
+    getAllReceive : function (req, res) {
+      var groupId = req.params.id;
+      Cluzt.find({receiver.id == groupId && send == true) {
+        if (err) {
+          res.send(err)
+        } else {
+          async.each(cluzts.sender, function(item, cb) {
+            Group.populate(item, { path: "sender" }, function(err, output) {
+              if (err) {
+                res.json({
+                  status:400,
+                  message: err
+                });
+              }
+              cb();
+            });
+          }, function(err) {
+            res.json({
+              status:200,
+              message: "Get Cluzt success",
+              data: cluzts
+            });
+          }
+        }
+      });
+    }
+}
+
+module.exports = cluzt;
